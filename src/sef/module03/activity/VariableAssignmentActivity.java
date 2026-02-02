dca0282d5974451c69d312a45cbd651355318010
Gayantha Chamith <73245735+gayanthachamith@users.noreply.github.com>
2026-02-02 19:57:59 +0200

    activity 01

---
+++ src/sef/module03/activity/VariableAssignmentActivity.java
@@ -0,0 +1,15 @@
+package sef.module03.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+
+        // Declare an int variable and assign a default value
+        int number = 0;
+
+        // Update the value
+        number = 25;
+
+        // Print the updated value
+        System.out.println("Updated value: " + number);
+    }
+}
